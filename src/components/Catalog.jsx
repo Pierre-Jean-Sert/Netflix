f3067056de4461dc0bfe3a3eca6b0932508011e6
Pierre-Jean <pjsert.edu@gmail.com>
2024-11-13 10:41:13 +0100

    optimisations mineures

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -4,26 +4,21 @@ function Catalog(props) {
   const result = filmsTab.map((elem) => {
     return (
       <>
-        <section className="container">
+        <section key={elem.catagory} className="container">
           {/* Titres des catégories */}
           <h2>{elem.category}</h2>
 
+          {/* Images des films  */}
           <div className="movies-list">
-            {/* Images des films  */}
-            {elem.images.map((elm) => {
-              console.log(elm);
-              return (
-                <div>
-                  <img src={elm} />
-                </div>
-              );
+            {elem.images.map((elem) => {
+              return <img key={elem} src={elem} alt="Movie" />;
             })}
           </div>
         </section>
       </>
     );
   });
-
+  //On retourne le résultat des deux mappages stockés dans result
   return result;
 }
 
